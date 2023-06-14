0e7f836a3a23e15d111dfc409cd01960d362d2d0
Tony Chen <londonbolo@gmail.com>
2023-06-14 12:45:57 +1200

    Added about message

--- Create_MH_Library.py
+++ Create_MH_Library.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QFileDialog, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QFileDialog, QMessageBox, QAction
 import lib_transfer as lt
 
 '''
@@ -57,7 +57,29 @@ class MainWindow(QMainWindow):
         create_library.move(30, 240)
         create_library.clicked.connect(self.create_library)
 
+                # Create the help menu
+        self.help_menu = self.menuBar().addMenu("Help")
+
+        # Create the about action
+        self.about_action = QAction("About", self)
+        self.about_action.triggered.connect(self.show_about_message)
+
+        # Add the about action to the help menu
+        self.help_menu.addAction(self.about_action)
+
         self.show()
+
+    def show_about_message(self):
+        QMessageBox.about(self, "About", "MassHunter Library creator for MassOmics.\n Tony Chen\n 2023\nhttps://github.com/boloson/OmicHelper")
+        # # Create a QMessageBox object
+        # msg_box = QMessageBox()
+
+        # # Set the message text
+        # msg_box.setText("This is a sample PyQt5 application.")
+
+        # # Show the message box
+        # msg_box.show()
+
 
     def browse_mass_hunter(self):
         options = QFileDialog.Options()
@@ -83,17 +105,19 @@ class MainWindow(QMainWindow):
                 file_name = file_name+".mslibrary.xml"
             self.output_file_input.setText(file_name)
 
-    def add_suffix(self):
+    def add_suffix(self, file_name):
         file_name = self.output_file_input.text()
-        if not file_name.endswith('.ms'):
-            self.output_file_input.setText(file_name + '.ms')
+        if not file_name.endswith('.mslibrary.xml'):
+            file_name = file_name + '.mslibrary.xml'
+        return file_name
     
 
     def create_library(self):
         # print(self.mass_hunter_input.text())
         mass_hunter_lib = self.mass_hunter_input.text()
         summary_report = self.mass_omic_input.text()
-        output_file = self.output_file_input.text()
+        output_file = self.add_suffix(self.output_file_input.text())
+
         try:
             lt.create_library_from_summary(mass_hunter_lib, summary_report, output_file)
             QMessageBox.information(self, 'Success', 'A Mass Hunter library was successfully created at the location: {}'.format(output_file))
